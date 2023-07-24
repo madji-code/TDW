2e233cffe6dace721b28b79c9fddd5683fdd0252
Joseph <j.a.d.ahouanto@gmail.com>
2023-07-24 19:00:44 -0400

    first glanced website for 3D prints sells.

--- object3d/vente/admin.py
+++ object3d/vente/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Object
+from .models import *
 
 admin.site.register(Object)
+admin.site.register(User)
--- object3d/vente/urls.py
+++ object3d/vente/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('view-objects', views.view_objects, name="view-objects"),
     path('view-this-object/<int:id>', views.view_this_object, name="view-this-object"),
+    path('panier/<int:user_id>', views.panier, name="panier"),
+    path('connexion', views.connexion, name="connexion"),
+    path('create-account', views.create_account, name="create-account"),
 ]
 
